265649ad256e484a73ee4ec18d4acda0f684c0bb
ege-ozd <egeayardogan@gmail.com>
2026-01-31 16:40:05 +0300

    cleaner.py

--- src/analysis/cleaner.py
+++ src/analysis/cleaner.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 class TurkishDataCleaner:
     def __init__(self):
-        # Stopword listesini buraya ekledik (İleride büyüteceğiz)
+        # Stopword listesi
         self.stopwords = ["ve", "bir", "bu", "da", "de", "için", "en", "mi", "mı", "mu", "ki", "ise", "ile", "gibi", "o", "şu", "ben", "biz", "siz", "onlar"]
 
     def turkish_lower(self, text):
-        """Türkçe karakterlere duyarlı küçük harf dönüşümü (Garantici yöntem)"""
+        
         if not isinstance(text, str): return ""
-        # .lower() her zaman İ -> i yapamaz, o yüzden manuel eşliyoruz
+        
         mapping = {"İ": "i", "I": "ı", "Ş": "ş", "Ğ": "ğ", "Ü": "ü", "Ö": "ö", "Ç": "ç"}
         for key, value in mapping.items():
             text = text.replace(key, value)
@@ -23,18 +23,18 @@ class TurkishDataCleaner:
         # 1. Küçük harf dönüşümü
         text = self.turkish_lower(text)
 
-        # 2. URL, E-posta ve Sayıları temizle
+        # 2. URL, E-posta ve Sayıları temizleme
         text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
         text = re.sub(r'\S+@\S+', '', text)
         text = re.sub(r'\d+', '', text)  # Sayıları da temizliyoruz (istatistiği bozmasın)
 
-        # 3. Özel karakterleri ve Emojileri temizle (Sadece harf ve boşluk)
+        # 3. Özel karakterleri ve Emojileri temizleme
         text = re.sub(r'[^a-zıığüşöç\s]', ' ', text)
 
-        # 4. Fazla boşlukları temizle
+        # 4. Fazla boşlukları temizleme
         text = re.sub(r'\s+', ' ', text).strip()
 
-        # 5. Stopword Temizliği (Opsiyonel: Kelime bazlı analiz yapacaksan ekle)
+        # 5. Stopword Temizliği
         text = " ".join([word for word in text.split() if word not in self.stopwords])
 
         return text
